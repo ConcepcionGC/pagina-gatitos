7c327eca833d9b68c85ae0d56d54019b9312646a
ConcepcionGC <128764837+ConcepcionGC@users.noreply.github.com>
2023-04-11 18:42:59 -0600

    Add files via upload

---
+++ algoritmos1.py
@@ -0,0 +1,36 @@
+# Realizar un algoritmo que sume dos numeros
+
+    Inicio suma{
+      Leer num1
+      Leer num2
+      suma=num1+num2
+      Escribir "La suma de", num1 "y" num2 "es igual a", suma
+    }
+
+
+
+#Realizar un algoritmo que permita leer dos valores distintos 
+#determinar cual de los dos valores es el mayor y escribirlo
+    Inicio mayorque{
+        Leer num1
+        Leer num2
+
+        si num1>num2 entonces--VERDADERO
+        Escribir "El número mayor es: ", num1
+        SINO--FALSO
+        Escribir "El numero mayor es ", num2
+        Fin--VERDADERO
+    }
+
+#Realizar un algoritomo que permita leer dos valores distintos,
+#determina cual de los dos valores es el menor y escribelo
+ Inicio mayorque{
+        Leer num1
+        Leer num2
+
+        si num1<num2 entonces--VERDADERO
+        Escribir "El número menor es: ", num1
+        SINO--FALSO
+        Escribir "El numero menor es ", num2
+        Fin--VERDADERO
+    }
